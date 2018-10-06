fedddb8183d02884b18b4653e2d1610cc79d3fcb
Jared <jared@updatepromise.com>
2018-10-05 23:18:14 -0700

    ..

--- app/app.py
+++ app/app.py
@@ -4,19 +4,55 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+class RestfulHandler(object):
+  def response(self, request_id, **kwargs):
+    payload = {'jsonrpc': '2.0',
+                'id': request_id,
+                'result': kwargs}
+    return json.dumps(payload)
+
+    self.log("Sent: %s" % payload_js)
+
+  def process(self, msg):
+    payload = json.loads(msg)
+    method_signature = 'handle_%s' % payload['method']
+    result = getattr(self, method_signature)(**payload['params'])
+    if result:
+      return self.response(payload['id'], **result)
+
+
+def requiresAuth(func):
+  def wrapper(self, session_token, **params):
+    self.user_id = None
+    self.session_token = session_token
+    session = {'iLoginID' : 1}
+    if session:
+      self.user_id = session['iLoginID']
+      return func(self, **params)
+    else:
+      return {'isAuthenticated': False}
+  return wrapper
+
 # Instantiate the app
 app = Flask(__name__)
 api = Api(app)
 
-class Observatory(Resource):
+# class Handler(Resource):
+class Handler(RestfulHandler):
     def get(self):
         return {
             'Galaxies': ['Milkyway', 'Andromeda',
             'Large Magellanic Cloud (LMC)']
         }
 
+    @requiresAuth
+    def handle_getHelloWorld(self):
+        return {'hello_world': 'Hello World'}
+
+
 # Create routes
-api.add_resource(Observatory, '/')
+api.add_resource(Handler, '/')
+# api.add_resource(Handler, '/')
 
 # Run the application
 if __name__ == '__main__':
