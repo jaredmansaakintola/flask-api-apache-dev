d04e9c1b482094ec331ab7e96cbf5ef38c4ea210
Jared <jared@updatepromise.com>
2018-10-12 14:19:27 -0700

    flask container created next is apache webserver container and a link between them

--- app/app.py
+++ app/app.py
@@ -3,28 +3,10 @@
 # Import framework
 from flask import Flask
 from flask_restful import Resource, Api
-
-class RestfulHandler(object):
-  def response(self, request_id, **kwargs):
-    payload = {'jsonrpc': '2.0',
-                'id': request_id,
-                'result': kwargs}
-    return json.dumps(payload)
-
-    self.log("Sent: %s" % payload_js)
-
-  def process(self, msg):
-    payload = json.loads(msg)
-    method_signature = 'handle_%s' % payload['method']
-    result = getattr(self, method_signature)(**payload['params'])
-    if result:
-      return self.response(payload['id'], **result)
-
+from flask import jsonify
 
 def requiresAuth(func):
   def wrapper(self, session_token, **params):
-    self.user_id = None
-    self.session_token = session_token
     session = {'iLoginID' : 1}
     if session:
       self.user_id = session['iLoginID']
@@ -37,23 +19,29 @@ def requiresAuth(func):
 app = Flask(__name__)
 api = Api(app)
 
-# class Handler(Resource):
-class Handler(RestfulHandler):
-    def get(self):
-        return {
-            'Galaxies': ['Milkyway', 'Andromeda',
-            'Large Magellanic Cloud (LMC)']
+class Handler(Resource):
+    @requiresAuth
+    @app.route('/')
+    def get():
+        # return "Hello World"
+        animals = {
+            'Animals': [
+                'Wolf',
+                'Bat',
+                'Cat'
+            ]
         }
 
-    @requiresAuth
-    def handle_getHelloWorld(self):
-        return {'hello_world': 'Hello World'}
+        return jsonify(animals)
+
+        # @requiresAuth
+        # @app.route('/hello_world')
+        # def handle_getHelloWorld(self):
+        #     return {'hello_world': 'Hello World'}
 
 
-# Create routes
-api.add_resource(Handler, '/')
-# api.add_resource(Handler, '/')
+api.add_resource(Handler, '/') # Route_1
 
 # Run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', debug=True)
